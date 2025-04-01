937698bc7f2cffd31cf8ce6896c0db60b9ce5ee3
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-01 16:14:24 +0200

    Config head html

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width-device-width, user-scalable-1.0, initial-scale-1.0, maximum-scale-1.0, minimum-scale-1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" rel="stylesheet"
+              crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/js-data/3.0.11/js-data.min.js">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+        <title>CRUD AJAX</title>
+    </head>
+    <body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+                referrerpolicy="no-referrer"></script>
+
+    </body>
+</html>
