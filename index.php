3266849343aa31c0b846d14a2a75ef93cb309ebf
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-01 16:49:49 +0200

    config style

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width-device-width, user-scalable-1.0, initial-scale-1.0, maximum-scale-1.0, minimum-scale-1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" rel="stylesheet"
-              crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+<!--        Bootstrap CSS-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" crossorigin="anonymous">
+
+<!--        DataTables Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
+<!--        Font Awesome-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
         <title>CRUD AJAX</title>
@@ -54,7 +57,7 @@
             <div class="col-md-6">
                 <div class="d-flex justify-content-end">
                     <button class="btn btn-primary btn-sm me-3"><i class="fas fa-folder-plus"></i> Nouveau</button>
-                    <a href="" class="btn btn-success btn-sm" id="export"><i class="fas fa-table"></i> Exporter</a>
+                    <a href="#" class="btn btn-success btn-sm" id="export"><i class="fas fa-table"></i> Exporter</a>
                 </div>
             </div>
         </div>
