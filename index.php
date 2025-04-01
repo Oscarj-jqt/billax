9f3949f2d6d8de69b39125403166c3ecc4e1d669
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-01 16:19:01 +0200

    config and add database file

--- index.php
+++ index.php
@@ -15,6 +15,10 @@
     <body>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
                 referrerpolicy="no-referrer"></script>
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"
+                crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/js-data/3.0.11/js-data.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="process.js"></script>
     </body>
 </html>
