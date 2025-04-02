aa3bb45feec4fca50454ca3980abe13e66d9934c
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-02 13:44:49 +0200

    Add connection to dataabase system

--- process.php
+++ process.php
@@ -1,1 +1,19 @@
 <?php
+
+class Database
+{
+   private $host = "mysql:dbname=crud_ajax";
+   private $user = "root";
+   private $pswd = "";
+
+   private function getconnexion()
+   {
+       try {
+           return new PDO($this->host, $this->user, $this->pswd);
+       } catch (PDOException $e) {
+           die('Erreur dans la tentative de connexion à la base de donnée:' . $e->getMessage());
+       }
+   }
+
+
+}
