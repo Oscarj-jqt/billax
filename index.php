ba15d5af0c240d87694a3b76aaeb9fd6319e5f59
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-04-02 17:49:37 +0200

    add modal

--- index.php
+++ index.php
@@ -1,123 +1,174 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-<!--        Bootstrap CSS-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" crossorigin="anonymous">
 
-<!--        DataTables Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <!-- DataTables Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
-<!--        Font Awesome-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
-        <title>CRUD AJAX</title>
-    </head>
-    <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><i class="fas fa-user-secret"> </i>Coding City</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+    <title>CRUD AJAX</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><i class="fas fa-user-secret"></i> Coding City</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(en cours)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Une autre action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Autre chose</a>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Désactivé</a>
-                            </li>
-                        </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(en cours)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Une autre action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Autre chose</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Désactivé</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<section class="container py-5">
+    <div class="row">
+        <div class="col-lg-8 col-sm mb-5 mx-auto">
+            <h1 class="fs-4 text-center lead text-primary">CRUD PHP POO MYSQL AJAX</h1>
+        </div>
+    </div>
+
+    <div class="dropdown-divider border-warning"></div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <h5 class="fw-bold mb-0">Liste des factures</h5>
+        </div>
+        <div class="col-md-6">
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-primary btn-sm me-3" data-bs-toggle="modal" data-bs-target="#createModal"><i class="fas fa-folder-plus"></i> Nouveau</button>
+                <a href="#" class="btn btn-success btn-sm" id="export"><i class="fas fa-table"></i> Exporter</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="dropdown-divider border-warning"></div>
+
+    <div class="row">
+        <div class="table-responsive" id="orderTable">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Client</th>
+                    <th scope="col">Caissier</th>
+                    <th scope="col">Montant</th>
+                    <th scope="col">Perçu</th>
+                    <th scope="col">Retourné</th>
+                    <th scope="col">Etat</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php for($i = 0; $i < 100; $i++): ?>
+                    <tr>
+                        <th scope="row"><?= $i ?></th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>
+                            <a href="#" class="text-info me-2 infoBtn" title="Voir détails"><i class="fas fa-info-circle"></i></a>
+                            <a href="#" class="text-primary me-2 editBtn" title="Modifier"><i class="fas fa-edit"></i></a>
+                            <a href="#" class="text-danger me-2 deleteBtn" title="Supprimer"><i class="fas fa-trash-alt"></i></a>
+                        </td>
+                    </tr>
+                <?php endfor; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
+<!-- Modal -->
+<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createModalLabel">Nouvelle facture</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post" id="formOrder">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="customer" name="customer">
+                        <label for="customer">Nom du client</label>
                     </div>
-                </div>
-            </nav>
-        </header>
-        <section class="container py-5">
-            <div class="row">
-                    <div class="col-lg-8 col-sm mb-5 mx-auto">
-                        <h1 class="fs-4 text-center lead text-primary">CRUD PHP POO MYSQL AJAX</h1>
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="cashier" name="cashier">
+                        <label for="cashier">Nom du caissier</label>
                     </div>
-                </div>
-            <div class="dropdown-divider border-warning"></div>
-            <div class="row">
-                    <div class="col-md-6">
-                        <h5 class="fw-bold mb-0">Liste des factures</h5>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="d-flex justify-content-end">
-                            <button class="btn btn-primary btn-sm me-3"><i class="fas fa-folder-plus"></i> Nouveau</button>
-                            <a href="#" class="btn btn-success btn-sm" id="export"><i class="fas fa-table"></i> Exporter</a>
+                    <div class="row g-2">
+                        <div class="col-md">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="amount" name="amount">
+                                <label for="amount">Montant</label>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <div class="dropdown-divider border-warning"></div>
-            <div class="row">
-                    <div class="table-responsive" id="orderTable">
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">CLient</th>
-                                <th scope="col">Caissier</th>
-                                <th scope="col">Montant</th>
-                                <th scope="col">Perçu</th>
-                                <th scope="col">Retourné</th>
-                                <th scope="col">Etat</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <?php for($i = 0; $i < 100; $i++): ?>
-                                <tr>
-                                    <th scope="row"><?= $i ?></th>
-
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                    <td>
-                                        <a href="#" class="text-info me-2 infoBtn" title="Voir détails"><i class="fas
-                                        fa-info-circle"></i></a>
-                                        <a href="#" class="text-primary me-2 editBtn" title="Modifier"><i class="fas
-                                        fa-edit"></i></a>
-                                        <a href="#" class="text-danger me-2 deleteBtn" title="Supprimer"><i class="fas
-                                        fa-trash-alt"></i></a>
-                                    </td>
-                                </tr>
-                                <?php endfor; ?>
-                            </tbody>
-                        </table>
+                    <div class="col-md">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="receive" name="receive">
+                            <label for="receive">Montant perçu</label>
+                        </div>
                     </div>
-                </div>
-            <div class="row"></div>
-        </section>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-                referrerpolicy="no-referrer"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"
-                crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/js-data/3.0.11/js-data.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script src="process.js"></script>
-    </body>
+                    <div class="col-md">
+                        <div class="form-floating mb-3">
+                            <select class="form-select" id="state" aria-label="state" name="state">
+                                <option value="Facturée">Facturée</option>
+                                <option value="Payée">Payée</option>
+                                <option value="Annulée">Annulée</option>
+                            </select>
+                            <label for="state">Etat</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                <button type="button" class="btn btn-primary">Ajouter <i class="fas fa-plus"></i> </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/js-data/3.0.11/js-data.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="process.js"></script>
+</body>
 </html>
